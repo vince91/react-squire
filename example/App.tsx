a46d0f4396f2ce6bde145b2f0c00a7476b112f2f
Vincent Timofti <vincent.timofti@gmail.com>
2018-02-15 19:02:16 +0100

    Add example files

---
+++ example/App.tsx
@@ -0,0 +1,5 @@
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+import Squire from "../src/components/Editor";
+
+ReactDOM.render(<Squire />, document.getElementById("root"));

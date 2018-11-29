0f08d62b081fc9109caa28fa729d2f3a7dccf783
Vincent Timofti <vincent.timofti@gmail.com>
2018-11-29 19:52:01 +0100

    wip

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,10 +4,15 @@ import Action from "./Action";
 
 function testPresenceinSelection(
   editor: Editor,
-  format: string,
-  validation: RegExp
+  validation: RegExp,
+  tag: string,
+  attributes: any = {}
 ) {
-  return validation.test(editor.getPath()) || editor.hasFormat(format);
+  console.log(
+    validation.test(editor.getPath()),
+    editor.hasFormat(tag, attributes)
+  );
+  return validation.test(editor.getPath()) || editor.hasFormat(tag, attributes);
 }
 
 function toggleAction(
@@ -17,11 +22,28 @@ function toggleAction(
   format: string,
   validation: RegExp
 ) {
-  if (testPresenceinSelection(editor, format, validation)) {
+  if (testPresenceinSelection(editor, validation, format)) {
     (editor as any)[removeFn]();
   } else {
     (editor as any)[activeFn]();
     editor.focus();
+  }
+}
+
+function toggleHeading(editor: Editor, size: string) {
+  if (
+    testPresenceinSelection(
+      editor,
+      new RegExp(`>SPAN.size\\[fontSize=${size}\\]>B\\b`),
+      "SPAN",
+      { class: "size", style: `font-size: ${size}` }
+    )
+  ) {
+    editor.removeBold();
+    editor.setFontSize();
+  } else {
+    editor.bold();
+    editor.setFontSize(size);
   }
 }
 
@@ -42,8 +64,34 @@ export default class Squire extends React.Component {
   toggleUnderline = () =>
     toggleAction(this.editor, "underline", "removeUnderline", "U", />U\b/);
 
+  toggleUnorderedList = () =>
+    toggleAction(this.editor, "makeUnorderedList", "removeList", "UL", />UL\b/);
+
+  toggleOrderedList = () =>
+    toggleAction(this.editor, "makeOrderedList", "removeList", "OL", />OL\b/);
+
+  toggleH1 = () => {
+    toggleHeading(this.editor, "2em");
+  };
+
+  toggleH2 = () => {
+    toggleHeading(this.editor, "1.5em");
+  };
+
+  toggleH3 = () => {
+    toggleHeading(this.editor, "1.2em");
+  };
+
+  clearFormatting = () => {
+    this.editor.removeAllFormatting();
+  };
+
   componentDidMount() {
     this.editor = new Editor(this.editorDiv);
+
+    this.editor.addEventListener("pathChange", () =>
+      console.log(this.editor.getPath())
+    );
   }
 
   componentWillUnmount() {
@@ -55,9 +103,21 @@ export default class Squire extends React.Component {
       <div className="squire">
         <div className="squire-toolbar">
           <div className="squire-actionGroup">
+            <Action onClick={this.toggleH1}>H1</Action>
+            <Action onClick={this.toggleH2}>H2</Action>
+            <Action onClick={this.toggleH3}>H3</Action>
+          </div>
+          <div className="squire-actionGroup">
             <Action onClick={this.toggleBold}>Bold</Action>
             <Action onClick={this.toggleItalic}>Italic</Action>
             <Action onClick={this.toggleUnderline}>Underline</Action>
+          </div>
+          <div className="squire-actionGroup">
+            <Action onClick={this.toggleUnorderedList}>Unordered list</Action>
+            <Action onClick={this.toggleOrderedList}>Ordered list</Action>
+          </div>
+          <div className="squire-actionGroup">
+            <Action onClick={this.clearFormatting}>Clear</Action>
           </div>
         </div>
         <div className="squire-editor" ref={this.setEditorDiv} />
--- src/global.d.ts
+++ src/global.d.ts
@@ -2,7 +2,7 @@ declare module "squire-rte" {
   export default class Editor {
     constructor(div: HTMLDivElement);
     getPath(): any;
-    hasFormat(type: string): any;
+    hasFormat(type: string, props?: any): any;
     removeBold(): void;
     removeItalic(): void;
     removeUnderline(): void;
